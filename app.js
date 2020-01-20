23afa2f7c47abb50ba8c6a553db3ea6607abd0a7
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2020-01-20 18:46:30 -0500

    implement the post and home route to server

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express = require('express'),
       mongoose = require('mongoose'),
       app = express();
 
-app.get('/', (req, res) => {
-    res.send('you are home')
-})
+//home page
+const homeRoute = require('./routes/home')
 
+app.use('/', homeRoute);
+
+
+//for the post page
+const postRoute = require('./routes/post')
+
+app.use('/', postRoute)
+
+//set the port to access the site on
 app.listen(3000);
 
-mongoose.connect()
+// mongoose.connect()
