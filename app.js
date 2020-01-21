bc711fbdb21dbadf15f4c94c4c46d213c5b995a9
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2020-01-20 19:47:24 -0500

    add json parsing for post request

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+require('dotenv/config');
+
 const express = require('express'),
       mongoose = require('mongoose'),
       app = express();
+
+      app.use(express.json());
+
+      
 
 //home page
 const homeRoute = require('./routes/home')
@@ -11,9 +17,18 @@ app.use('/', homeRoute);
 //for the post page
 const postRoute = require('./routes/post')
 
-app.use('/', postRoute)
+app.use('/post', postRoute)
 
 //set the port to access the site on
-app.listen(3000);
 
-// mongoose.connect()
+const port = process.env.PORT; //get the port number from the env file
+
+app.listen(port, () => {
+    console.log(`Listening on ${port}`)
+})
+
+mongoose.connect(process.env.DB_URI, {useUnifiedTopology:true, useNewUrlParser:true}, 
+    () => { 
+        console.log('Connected to database');
+        
+    })
