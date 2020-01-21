5af93d76d2f1ca2f321b7d8b883f869233cea6fe
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2020-01-20 19:47:49 -0500

    implement POST method

--- post.js
+++ post.js
@@ -1,10 +1,40 @@
 const express = require('express'),
-      route = express.Router();
+      route = express.Router(),
+
+      postSchema = require('../models/Post');
 
 route.get('/', (req, res) => {
     res.json({
         message: "you are on the post page"
     })
+})
+
+route.post('/', async (req, res) => {
+
+    const currPost = new postSchema({
+        title: req.body.title,
+        message: req.body.message
+    });
+
+    try {
+
+        const savedPost = await currPost.save() 
+
+        res.status(200).json({
+            yourpost: savedPost
+        })
+        
+    } catch (err) {
+
+        console.log(err);
+        
+
+        res.status(500).json({
+            message: err
+        })
+        
+    }
+
 })
 
 module.exports = route;
