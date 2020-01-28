b5d6d2878d404e1e35cef35124475d7e7af0ad47
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-01-27 21:52:26 -0500

    connect to mongodb atlas, add connection id to .env to use in server.js

--- RESTAPI1-27-2020/server.js
+++ RESTAPI1-27-2020/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+require('dotenv/config');
 
 
 app.get('/', (req, res) => {
+    console.log(process.env.DB_ID);
+    
     res.json({message: "hello"});
-})
+});
 
-app.listen(3000, () => console.log('Server listening'));
+const myMongoDBId = process.env.DB_ID;
+
+
+mongoose.connect(myMongoDBId, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.listen(3000, () => console.log('Server listening\nMDB ID: ', process.env.DB_ID));
