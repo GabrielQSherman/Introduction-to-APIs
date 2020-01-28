0f64bc332c25e1711e838f77650e25a1da0fd3c5
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-01-28 00:06:46 -0500

    adding a post page to server routes

--- RESTAPI1-27-2020/server.js
+++ RESTAPI1-27-2020/server.js
@@ -1,14 +1,25 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-
+//loads enviorment variables for server.js to use
 require('dotenv/config');
 
+const express = require('express'),
+      app = express(),
+      mongoose = require('mongoose'),
+
+      //all the route handling for the post page will be stored inside the postRouter variable, which is defined and then exported in the post.js file 
+      postRouter = require('./routes/post');
+
+//Middleware
+
+//for all routes; we are using json objects to format the body for request. so if we want to send data to the the mongo data base it will take a json object as input
+app.use(express.json);
+
+//MW for post route
+//if the user enters the /post route, then the postrouter variable will be called upon to handle http request
+app.use('/post', postRouter);
 
 app.get('/', (req, res) => {
-    console.log(process.env.DB_ID);
     
-    res.json({message: "hello"});
+    res.json({message: "hello, this is the home page"});
 });
 
 const myMongoDBId = process.env.DB_ID;
