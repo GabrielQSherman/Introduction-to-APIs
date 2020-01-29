8b255e160a84463780b9044f234b79c6d12fda41
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-01-29 00:20:18 -0500

    post request work for /subs page, succesfully added object into my database

--- RESTAPI1-27-2020/routes/subscribers.js
+++ RESTAPI1-27-2020/routes/subscribers.js
@@ -9,15 +9,38 @@ const subscriberSchema = require('../models/Sub');
       //Routes to create...
 
       //1. get all subs
-      router.get('/', (req, res) => {
+      router.get('/', async (req, res) => {
 
-        res.json({message: "you are looking at all subs"});
+        try {
+
+            const allSubs = await subscriberSchema.find();      
+
+            res.json(allSubs);
+
+        } catch (err) {
+
+            res.status(500).json({message: err.message}); //status 500 indicated serverside error
+        }
         
       });
 
       //2. create a sub
-      router.post('/', (req, res) => {
-        
+      router.post('/', async (req, res) => {
+
+            const newPost = subscriberSchema({
+                name: req.body.name,
+                subToChannel: req.body.subToChannel
+            });
+
+            try {
+
+                const savedPost = await newPost.save();
+
+                res.status(201).json(savedPost); //status 201 indicates an object has succesfully been created in the database
+                
+            } catch (err) {
+                res.status(400).json({message: err.message}); //status 400 indicates user/client gave bad data to server/api
+            }
       });
       
       //3. update a sub
