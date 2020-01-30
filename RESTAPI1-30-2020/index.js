ac7055c8cfa245471b03eeabc25753aa3855f161
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-01-30 18:24:28 -0500

    first get request working on home route

--- RESTAPI1-30-2020/index.js
+++ RESTAPI1-30-2020/index.js
@@ -11,3 +11,11 @@ const express = require('express'),
       mongoose = require('mongoose'),
 
       app = express();
+
+//handling the root route/home route
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.listen(3000, () => {console.log('Listening on Port 3000');
+})
