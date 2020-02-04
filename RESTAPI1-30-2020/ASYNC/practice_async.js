25ba6128088d05d8148a5e58f8c5d62cdaf4ef7a
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-02-03 20:20:21 -0500

    finished practice problem

--- RESTAPI1-30-2020/ASYNC/practice_async.js
+++ RESTAPI1-30-2020/ASYNC/practice_async.js
@@ -1,6 +1,21 @@
+const users = [
+    {
+    id: 1, 
+    name: 'gabe sherman', 
+    hasNetflix: true, 
+    email: 'email' },
+
+    {
+    id: 2, 
+    name: 'ben sherman', 
+    hasNetflix: false, 
+    email: 'email' }
+]
+
+//OLD CODE
 getCustomer(1, (customer) => {
     console.log('Customer: ', customer);
-    if (customer.isGold) {
+    if (customer.hasNetflix) {
       getTopMovies((movies) => {
         console.log('Top movies: ', movies);
         sendEmail(customer.email, movies, () => {
@@ -8,27 +23,55 @@ getCustomer(1, (customer) => {
         });
       });
     }
-  });
+  }); //this code will not work now that the functions simply return a promise. instead they are used to set variables that await the promise being finished
+
+  log_async_functions(1)
+
+  //NEW CODE
+  async function log_async_functions(id) { //'await' keyword must be used in a async function 
+
+    const customer = await getCustomer(id);
+    console.log('Customer: ', customer);
+    if (customer.hasNetflix) {
+        const movies = await getTopMovies()
+        console.log('Top movies: ', movies);
   
-  function getCustomer(id, callback) {
-    setTimeout(() => {
-      callback({ 
-        id: 1, 
-        name: 'Mosh Hamedani', 
-        isGold: true, 
-        email: 'email' 
-      });
-    }, 4000);  
+        const email = await sendEmail();
+        console.log('email sent!');
+        
+    }
   }
   
-  function getTopMovies(callback) {
-    setTimeout(() => {
-      callback(['movie1', 'movie2']);
-    }, 4000);
+  function getCustomer(id) {
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            let customer = users[id-1];
+            
+            resolve(customer); //send the correct user as an object
+
+        }, 1000);  
+    })
+    
   }
   
-  function sendEmail(email, movies, callback) {
-    setTimeout(() => {
-      callback();
-    }, 4000);
+  function getTopMovies() {
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(['Django Unchained', 'Iron Man']);
+        }, 1000);
+    })
+    
+  }
+  
+  function sendEmail(email, movies) {
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, 1000);
+    })
+    
   }
