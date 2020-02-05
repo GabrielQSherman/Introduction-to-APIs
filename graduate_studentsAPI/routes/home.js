924b00e600cd01a1821de3722f7406d17dbc1c71
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-02-05 12:39:36 -0500

    adding the schema for the students db in home.js, getting all documents in the database is important

--- graduate_studentsAPI/routes/home.js
+++ graduate_studentsAPI/routes/home.js
@@ -1,11 +1,44 @@
 const express = require('express'),
       router = express.Router(),
-      mongoose = require('mongoose');
+      mongoose = require('mongoose'),
+
+      StudentSchema = require('../models/students');
+
+      port = process.env.PORT;
+
+      
 
       router.get('/', (req, res) => {
 
-            res.status(200).json({message: "You are home"});
+            res.status(200).json({
+                message: "You are home",
+                options: `go to the http://localhost:${port}/find/all to see all graduates`
+            });
             
+      });
+
+      //all the post can be found individualy by id
+      router.get('/find/graduateid/:id', (req, res) => {
+
+
+
+      })
+
+      //all the post in the database
+      router.get('/find/all', async (req, res) => {
+
+        try {
+
+            allGrads = await StudentSchema.find();
+
+            res.send(allGrads)
+            
+        } catch (err) {
+
+            res.status(500).json({message: err});
+
+        }
+
       })
 
 
