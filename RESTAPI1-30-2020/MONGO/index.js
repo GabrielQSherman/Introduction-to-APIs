0a8097522e02d875331b99de84c8e0fffe3989f5
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-02-04 19:37:13 -0500

    added express to mongodb practice, can search for specific document in database

--- RESTAPI1-30-2020/MONGO/index.js
+++ RESTAPI1-30-2020/MONGO/index.js
@@ -1,9 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'),
+      express =  require('express'),
+      app = express();
 
-mongoose.connect('mongodb://localhost/testing') //this creates a promise
-    .then(() => console.log('Connected to Database')) //if everything goes well
-    .catch( err => console.log('Could not connect to database...', err)) //if not give an error to the log
-    
+
+    app.use(express.json())
+
     const courseSchema = mongoose.Schema({ 
         //this is model's 'object' constructor function it will set the blueprint for the objects that will be stored in the database
         name: String,
@@ -21,5 +22,52 @@ mongoose.connect('mongodb://localhost/testing') //this creates a promise
         tags: ['intro', 'photos101'],
     })
 
-    console.log(gabe_course);
+    // console.log(gabe_course);
+
+    // gabe_course.save()
+
+    app.get('/', (req, res) => {
+        res.send('hello')
+    })
+
+
+    app.get('/showcourses', async (req, res) => {
+
+        const allCourses = await Course.find();
+
+        res.send(allCourses)
+
+    })
+
+    app.get('/showcourses/:id', get_doc_by_id, (req, res) => {
+
+        console.log(req.document);
+        
+        res.send('complete')
+        // res.send(req.document)
+    })
+
+
+    async function get_doc_by_id(req, res, next) {
+
+        console.log(req.params.id);
+
+        let id = req.params.id
+        
+        req.document = await Course.findOne({_id: id});
+        
+        next()
+    }
+
+
+app.listen(3000, () => {
+
+    console.log('\nListening on 3000\n');
+    
+})
+
+
+mongoose.connect('mongodb+srv://user314:31415@cluster0-ichxa.mongodb.net/playground?retryWrites=true&w=majority', ( { useNewUrlParser: true , useUnifiedTopology: true})) //this creates a promise
+    .then(() => console.log('Connected to Database')) //if everything goes well
+    .catch( err => console.log('Could not connect to database...', err)) //if not give an error to the log
     
