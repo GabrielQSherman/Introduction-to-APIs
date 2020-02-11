c77d98a4707a8796af850425a4f8d67d4bf28529
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-02-11 00:23:40 -0500

    new post submitions show name of student and id on successful post

--- graduate_studentsAPI/database_frontend/admin.js
+++ graduate_studentsAPI/database_frontend/admin.js
@@ -34,15 +34,31 @@
                 body: postJson
             })
 
+            //after the fetch POST request goes to the server through the api and back to the client a promise is returned
+            //the resonse will be in a readable stream format(not good for extracting data)
+            //but it will have a status code that will be useful for error handling
             .then((response) => {
+                console.log(response.status);
+
+                if (response.status != 200) { //if a status other than 'OK' is receivedS
+
+                    console.log(response.json());
+                    
+                    throw new Error(`The students post failed Status: ${response.status}`)
+                    
+                }
+                
                 return response.json()
             })
+
+
+            //after the response is parsed to json its properties can be used in the DOM and a success message is sent to the client
             .then((json) => {
                 console.log(json.newpost);
 
-                document.getElementById('responseElm').innerText = json.newpost;
+                document.getElementById('responseElm').innerHTML = `New Student Added to Database<br>Student's Name: ${json.newpost.firstName} ${json.newpost.lastName}<br>Document DB Id: ${json.newpost._id}`;
 
-                document.getElementById('request_message').innerText = 'post submitied';
+                document.getElementById('request_message').innerText = 'Student Successfully Posted';
 
                 clear_formData(postForm) //sets all the input values to blank
                 
@@ -50,7 +66,9 @@
         
         } catch (err) {
 
-            console.log(err); 
+            document.getElementById('responseElm').innerText = err;
+
+            document.getElementById('request_message').innerText = 'Student Info Could Not Be Posted';
             
         }
         
