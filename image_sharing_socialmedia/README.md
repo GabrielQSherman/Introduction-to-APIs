5c6895e5ca6d7bc3ef8bcfb91c1ec9597b63f5b7
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-02-14 16:57:00 -0500

    express app can connect to mongodb

---
+++ image_sharing_socialmedia/README.md
@@ -0,0 +1,21 @@
+In this social media clone, cliental will be able to;
+
+    []create an account with password and username
+
+    []see any user via search and follow them if desired
+
+    []upload photolinks
+
+    []leave comments on photo post
+
+    []favorite photo post 
+
+
+    2/14/20
+
+        so far i have create an npm project, firstly i will work on my backend.
+        this will involve connecting to my mongodb account where user information will be stored. as well as their post in the future
+
+        [*] the server can connect to my cloud db 
+
+        [*] server is accessable via localhost (when server is run from the terminal)
--- image_sharing_socialmedia/index.js
+++ image_sharing_socialmedia/index.js
@@ -1,16 +1,54 @@
 //variables needed to run server
-
-
+require('dotenv/config')
 
 const express = require('express'),
       mongoose = require('mongoose'),
 
       app = express(); //instance of express (aka an express application)
 
+///////////////////////////////////////////////////////////////////////////////////////////// CONNECTING TO DATABASE 
+//Connect to my MongoDB account
 
-      let port = 3000;
+//get the database connection uri from my .env file
+let mongoUri = process.env.MOGO_URI;
+
+//connnect using the previously declared variable and use obj to pass through parameters so no depercation warnings occur
+mongoose.connect(mongoUri, ({ useNewUrlParser: true, useUnifiedTopology: true } ))
+
+//when the connection occurs these promises will fire
+mongoose.connection.on('connected', () => {
+
+    console.log(`\nMongoose connection open to ${mongoUri}\n`);
+
+});
+
+//if an error occurs, the a message will be logged
+mongoose.connection.on('error', (err) => { 
+
+    console.log(`Mongoose connection error: ${err}`);
+
+}); 
+
+//this will fire once the database has successfully made a connection
+mongoose.connection.once('open', () => {
+
+    console.log("\nDatabase Connected\n");
+
+})
+
+//if the DB disconnects a message will be logged
+mongoose.connection.on('disconnected', () => { 
+
+    console.log('\nMongoose default connection disconnected'); 
+
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////// CONNECTING TO LOCALHOST
+//Connect to the local host to run the server durring development
+let port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Server listening on Port ${port}`);
+    
+    console.log(`\nServer listening on Port ${port}`);
     
 })
