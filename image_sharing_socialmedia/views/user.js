83f56ff31c81b849a55318c3efe77a85aea3f645
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-03-21 18:23:15 -0400

    update profile js to take form data and return an object that will be passed to the body of a fetch api request to my api backend

--- image_sharing_socialmedia/views/user.js
+++ image_sharing_socialmedia/views/user.js
@@ -9,9 +9,51 @@ window.onload = () => {
 
     document.getElementById('picPostBtn').addEventListener('onclick', postPictureRequest);
 
-    function postPictureRequest() {
-        console.log('test');
+    async function postPictureRequest() {
+
+        let postJson = createPostJson('newPostForm'),
+
+        postRequestObj = {
+            
+            method: 'POST',
+
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+
+            body: postJson
+
+        };
+
+        console.log(postJson);
+        
+        
+        // await fetch('http://localhost:3000/user/newpost', postRequestObj)
+        
+        
+    }
+
+
+    function createPostJson(formID) {
+
+        let form = document.getElementById(formID),
+
+            postObj = {};
+
+        for (const inputBox of form) {
+
+            let name = inputBox.name;
+
+            postObj[name] = inputBox.value; 
+            
+        }
+
+        // console.log(form);
+        
+        return postObj
         
     }
 
 }
+
