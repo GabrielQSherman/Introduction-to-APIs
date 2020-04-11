ff0b311b597b5833d52669a2fb6ea0b03bd980ce
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-04-10 23:23:50 -0400

    implement like button request functions and going back to signin page request

--- image_sharing_socialmedia/public/publicprofile.js
+++ image_sharing_socialmedia/public/publicprofile.js
@@ -1,6 +1,44 @@
-document.getElementById('signin').onclick = signinRedirect;
 
-function signinRedirect() {
+
+window.onload = () => {
+
+    console.log('start');
     
-    location = 'http://localhost:3000/signin'
+
+
+    if (document.getElementById('signin') != null) {
+    
+    document.getElementById('signin').onclick = signinRedirect;
+
+    }
+
+    let allLikeButtons = document.getElementsByClassName('likebuttons');
+
+    if (allLikeButtons.length > 0) {
+
+        for (let i = 0; i < allLikeButtons.length; i++) {
+            
+            let img = allLikeButtons[i];
+
+            img.ondblclick = likeRequest;
+            
+        }
+        
+    }
+
+    function signinRedirect() {
+
+        // console.log(this);
+        
+        
+        location = 'http://localhost:3000/signin'
+    }
+
+    function likeRequest () {
+
+        console.log(this);
+        
+    }
+
+
 }
